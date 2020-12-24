8c37b5214e06d6e02f4f95564f79bc085cba8e64
Eric Blachère <eric.blachere.n7@gmail.com>
2020-12-24 18:43:46 +0100

    Add onChange modifier on reducers

--- Sources/ComposableArchitecture/Reducer+Utils.swift
+++ Sources/ComposableArchitecture/Reducer+Utils.swift
@@ -46,4 +46,69 @@ extension Reducer {
   }
 }
 
+extension Reducer {
+  
+  /// Creates a reducer that will perform work when a specific part of the state is altered.
+  ///
+  /// The main way to perfom work in TCA is to send a new action that the reducer will interpret
+  /// to modify state & maybe trigger a new action via an effect.
+  ///
+  /// However, sometimes it makes more sense for a change of some piece of state
+  /// to trigger a new action :
+  /// instead of triggering this new action at all points where the state if altered
+  /// in the reducer at the risk of forgetting one, we directly watch the interesting
+  /// substate using this modifier
+  ///
+  /// - Parameters:
+  ///   - value: Extracts the part of state that we want to watch for changes
+  ///   - isEqual: Condition to trigger the `onChange` closure
+  ///   - onChange: Performs work on the state and returns an effect based on current and previous
+  ///   values of the substate.
+  ///   Can be seen as a mini reducer where we react to a state change instead of an action
+  /// - Returns: A reducer that will call `onChange` when the piece of state returned by `value`
+  /// is altered by itself
+  public func onChange<Value>(
+    of value: @escaping (State) -> Value,
+    isEqual: @escaping (Value, Value) -> Bool,
+    _ onChange: @escaping (
+      _ initial: Value,
+      _ current: Value,
+      inout State,
+      Environment
+    ) -> Effect<Action, Never>
+  ) -> Reducer<State, Action, Environment> {
+    var initialValue: Value!
+    
+    return .combine(
+      Reducer { state, _, _ in
+        initialValue = value(state)
+        return .none
+      },
+      self,
+      Reducer { state, _, environment in
+        let currentValue = value(state)
+        
+        guard !isEqual(currentValue, initialValue) else {
+          return .none
+        }
+        
+        return onChange(initialValue, currentValue, &state, environment)
+      }
+    )
+  }
+  
+  /// Simplified version of `onChange(of:isEqual:_:)` where the substate is `Equatable`
+  public func onChange<Value: Equatable>(
+    of value: @escaping (State) -> Value,
+    _ onChange: @escaping (
+      _ initial: Value,
+      _ current: Value,
+      inout State,
+      Environment
+    ) -> Effect<Action, Never>
+  ) -> Reducer<State, Action, Environment> {
+    self.onChange(of: value, isEqual: ==, onChange)
+  }
+}
+
 
