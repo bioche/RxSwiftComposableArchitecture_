a2c61bd83608c4e4ae493174691c36b50541da63
Eric Blachère <eric.blachere.n7@gmail.com>
2020-12-24 17:43:28 +0100

    [TECH] move combined method and version 0.9.0

--- Sources/ComposableArchitecture/Reducer+Utils.swift
+++ Sources/ComposableArchitecture/Reducer+Utils.swift
@@ -4,11 +4,11 @@ extension Reducer {
   /// Creates a reducer that will transform the specified action (`case`) into another action (`embed`).
   ///
   /// Typically, this will allow for redirection of actions from parent into a child
-  /// ```
-  /// parentReducer
-  ///   .resending(ParentAction.fooClient, to: { ParentAction.child1(.fooClient($0)) })
-  ///   .resending(ParentAction.fooClient, to: { ParentAction.child2(.fooClient($0)) })
-  /// ```
+  ///
+  ///     parentReducer
+  ///       .resending(ParentAction.fooClient, to: { ParentAction.child1(.fooClient($0)) })
+  ///       .resending(ParentAction.fooClient, to: { ParentAction.child2(.fooClient($0)) })
+  ///
   ///
   /// - Parameters:
   ///   - case: The original action
@@ -28,6 +28,21 @@ extension Reducer {
         }
       }
     )
+  }
+  
+  /// Combines the current reducer with another one according to environment function
+  /// Allows for dynamic combining.
+  /// For example, we can condition the application of a reducer to a specific configuration
+  ///
+  ///      let fullReducer = partialReducer
+  ///       .combined { $0.includeAnalytics ? analyticsReducer : .empty }
+  ///
+  /// - Parameter fromEnvironment: A function which may return a reducer to combine with
+  /// - Returns: A single reducer.
+  public func combined(with fromEnvironment: @escaping (Environment) -> Reducer) -> Reducer {
+    Self { value, action, environment in
+      self.combined(with: fromEnvironment(environment))(&value, action, environment)
+    }
   }
 }
 
