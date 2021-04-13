8b0f0e4ffa5faf172fdf6ada6641703f74540a0c
Thibault Poujat <thibault.poujat@gmail.com>
2021-04-13 14:52:29 +0200

    [FIX] Add missing methods docs, add popover handling for iPad with both barButton and source view

--- Sources/ComposableArchitecture/UI/UIKit/UIAlertController+ActionSheetState.swift
+++ Sources/ComposableArchitecture/UI/UIKit/UIAlertController+ActionSheetState.swift
@@ -8,6 +8,7 @@ extension Store where State == ActionSheetState<Action>? {
   /// Presents or dismisses alert controller depending on state.
   /// The store will receive the actions emitted when pressing alert buttons
   /// - Parameter sheetPresenter: The controller doing the presenting
+  /// - Parameter sourceView: The source UIView needed for iPad popover display
   /// - Parameter onPresent: Can be used to customize alert controller UI or its popoverViewController
   /// - Returns: Disposable to cancel the subscription to store's state
   public func bindTo(sheetPresenter: UIViewController,
@@ -17,13 +18,37 @@ extension Store where State == ActionSheetState<Action>? {
     return ifLet(then: { [weak sheetPresenter] store in
       let viewStore = ViewStore(store, removeDuplicates: { _, _ in false })
       alertController = .from(viewStore: viewStore)
-      alertController?.modalPresentationStyle = .popover
-      sheetPresenter?.popoverPresentationController?.sourceView = sourceView
-      sheetPresenter?.popoverPresentationController?.sourceRect = sourceView.bounds
+      if let presenter = alertController?.popoverPresentationController {
+        presenter.sourceView = sourceView
+        presenter.sourceRect = sourceView.bounds
+      }
       onPresent(alertController!)
       sheetPresenter?.present(alertController!, animated: true, completion: nil)
-      }, else: { [weak alertController] in
-        alertController?.dismiss(animated: true, completion: nil)
+    }, else: { [weak alertController] in
+      alertController?.dismiss(animated: true, completion: nil)
+    })
+  }
+  
+  /// Presents or dismisses alert controller depending on state.
+  /// The store will receive the actions emitted when pressing alert buttons
+  /// - Parameter sheetPresenter: The controller doing the presenting
+  /// - Parameter barButtonItem: UIBarButtonItem the popover source needed for iPad display
+  /// - Parameter onPresent: Can be used to customize alert controller UI or its popoverViewController
+  /// - Returns: Disposable to cancel the subscription to store's state
+  public func bindTo(sheetPresenter: UIViewController,
+                     barButtonItem: UIBarButtonItem,
+                     onPresent: @escaping (UIAlertController) -> Void = { _ in }) -> Disposable {
+    var alertController: UIAlertController?
+    return ifLet(then: { [weak sheetPresenter] store in
+      let viewStore = ViewStore(store, removeDuplicates: { _, _ in false })
+      alertController = .from(viewStore: viewStore)
+      if let presenter = alertController?.popoverPresentationController {
+        presenter.barButtonItem = barButtonItem
+      }
+      onPresent(alertController!)
+      sheetPresenter?.present(alertController!, animated: true, completion: nil)
+    }, else: { [weak alertController] in
+      alertController?.dismiss(animated: true, completion: nil)
     })
   }
 }
